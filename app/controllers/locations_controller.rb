c2ca92e8da8a09b98c32d90c47ca8ce85e819b41
renanz <renan_zelaya@unitec.edu>
2018-02-19 22:37:06 -0600

    Add Devise

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,17 +2,5 @@ class LocationsController < ApplicationController
 
   def new
   end
-<<<<<<< HEAD
 
-  def create
-  end
-
-=======
-  
-  def create
-  	render plain: params[:location].inspect
-  end
-
-
->>>>>>> c7238025ff2becfc5e96550bcf6eb6c618fbbf83
 end
